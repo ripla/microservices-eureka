e8e63d168f90972ce6e1feff2bd174e95edb728a
Risto Yrjänä <risto@vaadin.com>
2015-03-06 20:22:44 +0200

    Don't expose DB id of Student

--- students-rest-microservice/src/main/java/org/vaadin/risto/microservices/student/StudentApplication.java
+++ students-rest-microservice/src/main/java/org/vaadin/risto/microservices/student/StudentApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @EnableDiscoveryClient
@@ -14,10 +13,5 @@ public class StudentApplication extends RepositoryRestMvcConfiguration {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentApplication.class, args);
-    }
-
-    @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.exposeIdsFor(Student.class);
     }
 }
